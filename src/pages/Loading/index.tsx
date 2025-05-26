1fe74fac3ed6c314c356369764b9feb5452d1b74
Kiet <kietnguyentuan911@gmail.com>
2025-05-26 14:39:06 +0700

    feat: remake Loading component

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,8 +1,20 @@
-const Loading = () => {
-    return (
-        <div className='flex justify-center items-center bg-mountain-50 w-full h-screen'>
-        </div>
-    )
-}
+import { Backdrop, CircularProgress, Typography } from "@mui/material"
+
+const Loading = () => (
+  <Backdrop
+    open
+    sx={{
+      zIndex: (theme) => theme.zIndex.modal + 1,
+      position: "fixed",
+      inset: 0, // shorthand for top:0; right:0; bottom:0; left:0
+      backgroundColor: "transparent",
+    }}
+  >
+    <CircularProgress color="inherit" />
+    <Typography variant="h6" sx={{ m: 2, color: "white" }}>
+      Processing...
+    </Typography>
+  </Backdrop>
+);
 
 export default Loading
